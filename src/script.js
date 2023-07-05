907b65e2c53d5e1f1ea64d45e4d1aa8a55f3dc32
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-07-05 17:04:37 +0500

    added dark mode

--- src/script.js
+++ src/script.js
@@ -8,13 +8,13 @@ const renderToHTML = (arr) => {
     let res = "";
     arr.map((data) => {
         res += `
-            <div class="border">
-                <img src="${data.flags.svg}" class="w-full h-[160px]">
+            <div class="shadow border border-[rgba(0, 0, 0, 0.03)] dark:border-[#2B3844] bg-white dark:bg-[#2B3844] transition-all">
+                <img src="${data.flags.svg}" class="w-full h-[160px] border border-white dark:border-[#2B3844]">
                 <div class="px-6 pt-6 pb-12">
-                    <h1 class="mb-4 text-[#111517] text-[18px] font-extrabold">${data.name}</h1>
-                    <li class="list-none mb-2 text-[#111517] text-sm font-semibold leading-4"><strong>Population:</strong>${data.population}</li>
-                    <li class="list-none mb-2 text-[#111517] text-sm font-semibold leading-4"><strong>Region:</strong>${data.region}</li>
-                    <li class="list-none text-[#111517] text-sm font-semibold leading-4"><strong>Capital:</strong>${data.capital}</li>
+                    <h1 class="mb-4 text-[#111517] text-[18px] font-extrabold dark:text-white">${data.name}</h1>
+                    <li class="list-none mb-2 text-[#111517] dark:text-white text-sm font-semibold leading-4"><strong>Population: </strong> ${data.population}</li>
+                    <li class="list-none mb-2 text-[#111517] dark:text-white text-sm font-semibold leading-4"><strong>Region: </strong> ${data.region}</li>
+                    <li class="list-none text-[#111517] dark:text-white text-sm font-semibold leading-4"><strong>Capital: </strong> ${data.capital}</li>
                 </div>
             </div>
         `
@@ -47,3 +47,22 @@ search.addEventListener("input", (e) => {
     let result = countries.filter((v) => v.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
     renderToHTML(result)
 })
+
+// -----------------Dark Mode --------------------
+
+
+const checkbox = document.getElementById('check')
+
+function darkMode() {
+    document.documentElement.classList.toggle("dark")
+}
+
+function onLoad(){
+    document.documentElement.classList.toggle("dark")
+}
+
+checkbox.addEventListener("change", () => {
+    darkMode()
+})
+
+document.addEventListener("DOMContentLoaded", onLoad)
